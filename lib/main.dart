d1bad7115db6263b6f09a263460dd3585989844e
SangHa,Lee <38284431+StrangeLee@users.noreply.github.com>
2020-09-03 18:04:11 +0900

    [FIX] UI update
    Cupertino 디자인과 Material 디자인을 구분해서 App에 적용 시킴

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,36 @@
 import 'package:diary_application/main_page.dart';
-import 'package:flutter/material.dart'; 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
+/*
+ * Flutter로 Android는 material 디자인으로 
+ * Ios는 cupertino 디자인으로 화면 구성을 할 것이다.
+ * 다이어리 앱으로 간단하게 적용해 볼 것이며
+ * 일기 추가, 삭제, 편집, 보기(다이얼로그)의 기능만 구현할 것이다.
+ */
+
+final materialThemeData = ThemeData(
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    accentColor: Colors.blue,
+    appBarTheme: AppBarTheme(color: Colors.blue.shade600),
+    primaryColor: Colors.blue,
+    secondaryHeaderColor: Colors.blue,
+    canvasColor: Colors.blue,
+    backgroundColor: Colors.red,
+    textTheme: TextTheme().copyWith(body1: TextTheme().body1));
+
+final cupertinoTheme = CupertinoThemeData(
+    primaryColor: Colors.blue,
+    barBackgroundColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white);
+
 void main() {
-  runApp(
-    PlatformApp(
+  runApp(PlatformApp(
     debugShowCheckedModeBanner: false,
+    android: (_) => MaterialAppData(theme: materialThemeData),
+    ios: (_) => CupertinoAppData(theme: cupertinoTheme),
     home: MainPage(),
     title: 'Diary Application',
   ));
--- lib/main_page.dart
+++ lib/main_page.dart
@@ -11,12 +11,85 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return PlatformScaffold(
+      iosContentPadding: true,
+      iosContentBottomPadding: true,
+      android: (context) => MaterialScaffoldData(
+        // android FAB
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
       appBar: PlatformAppBar(
         title: PlatformText('Diary App'),
+        ios: (context) => CupertinoNavigationBarData(
+            // ios NavigatonBar Button
+            transitionBetweenRoutes: false,
+            trailing: PlatformButton(
+              padding: EdgeInsets.all(4.0),
+              child: Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            )),
       ),
-      body: Center(
-        child: PlatformText('main_page'),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+      Padding(
+        padding: EdgeInsets.only(top: 5.0),
+        child: PlatformText(
+          '오늘 하루는 어땠나요?',
+        ),
       ),
+      SizedBox(
+        height: 10.0,
+      ),
+      Container(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            _diaryListItem(),
+            _diaryListItem(),
+            _diaryListItem(),
+          ],
+        ),
+        alignment: Alignment.center,
+      )
+    ]);
+  }
+
+  Widget _diaryListItem() {
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(color: Colors.black),
+      ),
+      child: Padding(
+          padding: EdgeInsets.fromLTRB(10, 8, 8, 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  PlatformText(
+                    'Title',
+                  ),
+                  PlatformText(
+                    'MM/dd',
+                    style: TextStyle(fontSize: 10.0),
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
