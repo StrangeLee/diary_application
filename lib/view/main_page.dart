468d0ba4a90953a7ec0477f95aa4cb152374ddb6
SangHa,Lee <38284431+StrangeLee@users.noreply.github.com>
2020-09-06 18:25:56 +0900

    [FIX] keyboard overflow error
    세로 길이가 작은 디바이스 사용시 textfiled가 키보드에 가리는 overflow 현상 해결

--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -28,34 +28,36 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return PlatformScaffold(
-      iosContentPadding: true,
-      iosContentBottomPadding: true,
-      material: (context, platform) => MaterialScaffoldData(
-        resizeToAvoidBottomInset: false,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _showAddDialog(context),
-          child: Icon(Icons.add),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: PlatformScaffold(
+        iosContentPadding: true,
+        iosContentBottomPadding: true,
+        material: (context, platform) => MaterialScaffoldData(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => _showAddDialog(context),
+            child: Icon(Icons.add),
+          ),
         ),
+        cupertino: (context, platform) => CupertinoPageScaffoldData(
+          resizeToAvoidBottomInset: false,
+        ),
+        appBar: PlatformAppBar(
+          title: PlatformText('Diary App'),
+          cupertino: (context, platform) => CupertinoNavigationBarData(
+              // ios NavigatonBar Button
+              transitionBetweenRoutes: false,
+              trailing: PlatformButton(
+                padding: EdgeInsets.all(4.0),
+                child: Icon(
+                  Icons.add,
+                  color: Colors.black,
+                ),
+                onPressed: () => _showAddDialog(context),
+              )),
+        ),
+        body: _body(),
       ),
-      cupertino: (context, platform) => CupertinoPageScaffoldData(
-        resizeToAvoidBottomInset: false,
-      ),
-      appBar: PlatformAppBar(
-        title: PlatformText('Diary App'),
-        cupertino: (context, platform) => CupertinoNavigationBarData(
-            // ios NavigatonBar Button
-            transitionBetweenRoutes: false,
-            trailing: PlatformButton(
-              padding: EdgeInsets.all(4.0),
-              child: Icon(
-                Icons.add,
-                color: Colors.black,
-              ),
-              onPressed: () => _showAddDialog(context),
-            )),
-      ),
-      body: _body(),
     );
   }
 
@@ -153,67 +155,63 @@ class _MainPageState extends State<MainPage> {
               height: maxLines * 40.0,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 15.0),
-                      child: PlatformText(
-                        '일기 저장',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 15.0),
+                        child: PlatformText(
+                          '일기 저장',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: ListView(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(bottom: 10),
-                            child: PlatformTextField(
-                              controller: titleTextController,
-                              material: (context, platform) =>
-                                  MaterialTextFieldData(
-                                      decoration: InputDecoration(
-                                          alignLabelWithHint: true,
-                                          hintText: '제목')),
-                              cupertino: (context, platform) =>
-                                  CupertinoTextFieldData(placeholder: '제목'),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(bottom: 10),
-                            child: PlatformTextField(
-                              controller: contentTextController,
-                              keyboardType: TextInputType.multiline,
-                              maxLines: 10,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 320.0,
-                            child: PlatformButton(
-                              color: const Color(0xFF1BC0C5),
-                              child: PlatformText(
-                                "Save",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              onPressed: () {
-                                if (contentTextController.text == '' ||
-                                    titleTextController.text == '') {
-                                  Toast.show('입력하지 않은 항목이 있습니다.', context);
-                                } else {
-                                  writeDiary(titleTextController.text,
-                                      contentTextController.text);
-                                  Navigator.pop(context);
-                                }
-                              },
-                            ),
-                          )
-                        ],
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: PlatformTextField(
+                          controller: titleTextController,
+                          material: (context, platform) =>
+                              MaterialTextFieldData(
+                                  decoration: InputDecoration(
+                                      alignLabelWithHint: true,
+                                      hintText: '제목')),
+                          cupertino: (context, platform) =>
+                              CupertinoTextFieldData(placeholder: '제목'),
+                        ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: PlatformTextField(
+                          controller: contentTextController,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 10,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 320.0,
+                        child: PlatformButton(
+                          color: const Color(0xFF1BC0C5),
+                          child: PlatformText(
+                            "Save",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {
+                            if (contentTextController.text == '' ||
+                                titleTextController.text == '') {
+                              Toast.show('입력하지 않은 항목이 있습니다.', context);
+                            } else {
+                              writeDiary(titleTextController.text,
+                                  contentTextController.text);
+                              Navigator.pop(context);
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
